5cf40ac0f06878b376e3137f86a8016006b11a1c
tfs_liyanpeng <liyanpeng@ruanmei.com>
2021-04-06 10:37:56 +0800

    fix

---
+++ Sources/Core/YPCoreKit.h
@@ -0,0 +1,26 @@
+//
+//  YPCoreKit.h
+//  YPCoreKit
+//
+//  Created by Else on 2021/4/6.
+//
+
+#ifndef YPCoreKit_h
+#define YPCoreKit_h
+
+#if __has_include(<YPCoreKit/YPCoreKit.h>)
+
+// MARK: Core
+#if __has_include(<YPCoreKit/YPCoreKit.h>)
+#import <YPCoreKit/YPCoreKit.h>
+#endif
+
+#else
+
+#if __has_include("YPCoreKit.h")
+#import "YPCoreKit.h"
+#endif
+
+#endif /* __has_include */
+
+#endif /* YPCoreKit_h */
